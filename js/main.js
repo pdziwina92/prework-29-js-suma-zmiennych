aeadd1c6be1f7e5462f370ab293f292f33223f67
paweldziwina <paweldziwina@gmail.com>
2024-03-04 14:08:59 +0100

    pierwsza zmiana

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let firstNumber = 4;
+
+let secondNumber = 2;
+
+function suma(num1, num2) {
+    let result = num1 + num2;
+    return result
+}
+
+suma(firstNumber, secondNumber)
+
+console.log( suma(firstNumber, secondNumber) );
+
